0e16e42b34584f8bf85bac4959f1a71f33a3428a
ezeid <ezeid@despegar.com>
2014-11-12 16:31:52 -0300

    adding hashcode and equals ti feature

--- app/models/Feature.java
+++ app/models/Feature.java
@@ -1,6 +1,8 @@
 package models;
 
 import com.google.gson.annotations.Expose;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
 import play.db.jpa.Model;
 
 import javax.persistence.CascadeType;
@@ -20,5 +22,24 @@ public class Feature extends Model {
     @Override
     public String toString() {
         return "{ \"id\" :  " + this.id +", \"description\": \"" + description + "\"  }";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) { return false; }
+        if (obj == this) { return true; }
+        if (obj.getClass() != getClass()) {
+            return false;
+        }
+        Feature rhs = (Feature) obj;
+        return new EqualsBuilder()
+                .appendSuper(super.equals(obj))
+                .append(description, rhs.description)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(description).toHashCode();
     }
 }
