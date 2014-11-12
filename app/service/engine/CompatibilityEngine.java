444cfaff78a4691cf992f8fc330a3335b73e0622
ezeid <ezeid@despegar.com>
2014-11-12 16:58:20 -0300

    changing compatibility engine

--- app/service/engine/CompatibilityEngine.java
+++ app/service/engine/CompatibilityEngine.java
@@ -1,11 +1,14 @@
 package service.engine;
 
+import com.google.common.collect.Maps;
 import models.Component;
 import models.ComponentFeature;
 import models.Part;
 import models.PartFeature;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.collections.Predicate;
+
+import java.util.Map;
 
 /**
  * @author ezeid on 7/2/14.
@@ -21,18 +24,23 @@ public class CompatibilityEngine {
     }
 
     public boolean areThereCompatibility(Part part, Component component) {
+        Map<PartFeature, Boolean> equalsMap = Maps.newHashMap();
         for (int i = 0; i < part.partFeature.size(); i++) {
-            final PartFeature partFeature = part.partFeature.get(i);
-            boolean exists = CollectionUtils.exists(component.compatibility, new Predicate() {
-                @Override
-                public boolean evaluate(Object o) {
-                    ComponentFeature componentFeature = (ComponentFeature) o;
-                    System.out.println("Component Specification : " + componentFeature.specification + " Part soecification: " + partFeature.specification);
-                    System.out.println("Component feature value: " +  componentFeature.value + " Part feature value: " + partFeature.value);
-                    return componentFeature.specification.equals(partFeature.specification) && componentFeature.value == partFeature.value;
+            PartFeature partFeature = part.partFeature.get(i);
+            equalsMap.put(partFeature, Boolean.FALSE);
+            for (ComponentFeature componentFeature : component.compatibility){
+                if (componentFeature.specification.equals(partFeature.specification)  && componentFeature.value == partFeature.value) {
+                    equalsMap.put(partFeature, Boolean.TRUE);
+                    break;
                 }
-            });
-            if (!exists) return false;
+            }
+
+        }
+        for (Boolean compatibility : equalsMap.values()) {
+            System.out.println(compatibility);
+            if(!compatibility) {
+                return compatibility;
+            }
         }
         return true;
     }
